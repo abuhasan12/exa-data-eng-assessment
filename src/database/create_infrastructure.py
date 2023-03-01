6f6b5f4f9e050648ac5c5f20f1fe73d8116ba3dc
unknown <abu_h@live.co.uk>
2023-03-01 15:44:22 +0000

    got rid of custom postgresql client

--- src/database/create_infrastructure.py
+++ src/database/create_infrastructure.py
@@ -1,23 +1,41 @@
 import argparse
-from src.utils import PostgresClient
+import psycopg2
 
-def create_database(client, database_sql):
-    client.connect_default()
-    client.enable_autocommit()
-    client.add_sql_file(database_sql)
-    client.commit_sql()
-    client.close_connection()
+def create_database(server_config, database_sql):
+    conn = psycopg2.connect(
+        host=server_config['host'],
+        port=server_config['port'],
+        user=server_config['user'],
+        password=server_config['password']
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+    with open (database_sql, 'r') as f:
+        create_db_sql = f.read()
+    cursor.execute(create_db_sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
     
-def create_tables(client, tables_sql, database):
-    client.switch_database(database=database)
-    client.connect()
-    client.add_sql_file(tables_sql)
-    client.commit_sql()
-    client.close_connection()
+def create_tables(server_config, tables_sql, database):
+    conn = psycopg2.connect(
+        host=server_config['host'],
+        port=server_config['port'],
+        user=server_config['user'],
+        password=server_config['password'],
+        database=database
+    )
+    cursor = conn.cursor()
+    with open (tables_sql, 'r') as f:
+        create_tables_sql = f.read()
+    cursor.execute(create_tables_sql)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-def create_infrastructure(client, new_database='fhir_database', database_sql='src/database/sql/database.sql', tables_sql='src/database/sql/tables.sql'):
-    create_database(client=client, database_sql=database_sql)
-    create_tables(client=client, tables_sql=tables_sql, database=new_database)
+def create_infrastructure(server_config, new_database='fhir_database', database_sql='src/database/sql/database.sql', tables_sql='src/database/sql/tables.sql'):
+    create_database(server_config=server_config, database_sql=database_sql)
+    create_tables(server_config=server_config, tables_sql=tables_sql, database=new_database)
 
 def main():
     """
@@ -26,8 +44,8 @@ def main():
     # Get args
     args = get_args()
 
-    client = PostgresClient(host=args.host, port=args.port, user=args.user, password=args.password, database=args.database)
-    create_infrastructure(client=client, new_database=args.new_database, database_sql=args.db_sql, tables_sql=args.tables_sql)
+    server_config = {'host': args.host, 'port': args.port, 'user': args.user, 'password': args.password}
+    create_infrastructure(server_config=server_config, new_database=args.new_database, database_sql=args.db_sql, tables_sql=args.tables_sql)
 
 def get_args() -> argparse.ArgumentParser:
     """
@@ -58,7 +76,7 @@ def get_args() -> argparse.ArgumentParser:
     )
     parser.add_argument(
         '--database', type=str, required=False, default=None,
-        help='A database to connect to.'
+        help='A database to connect to. Not required.'
     )
     parser.add_argument(
         '--new-database', type=str, required=False, default='fhir_database',
